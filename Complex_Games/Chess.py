f977641a58b15bebf581e4f8805021c34403450e
Perpetual <83892937+Flightycoder@users.noreply.github.com>
2022-06-09 22:00:16 -0700

    Update Chess.py

--- Complex_Games/Chess.py
+++ Complex_Games/Chess.py
@@ -1,3 +1,4 @@
+# Converts columns to rows on the chess board
 chess_map_from_alpha_to_index = {
    "a" : 0,
    "b" : 1,
@@ -8,6 +9,7 @@ chess_map_from_alpha_to_index = {
    "g" : 6,
    "h" : 7
 }
+# Converts rows to colums on the chess board
 chess_map_from_index_to_alpha = {
    0: "a",
    1: "b",
@@ -18,6 +20,8 @@ chess_map_from_index_to_alpha = {
    6: "g",
    7: "h"
 }
+
+# Gets the possible knight moves given the position
 def getKnightMoves(pos, chessBoard):
     column, row = list(pos.strip().lower())
     row = int(row) - 1
@@ -69,10 +73,31 @@ def getKnightMoves(pos, chessBoard):
     temp = [i for i in solutionMoves if i[0] >=0 and i[1] >=0]
     allPossibleMoves = ["".join([chess_map_from_index_to_alpha[i[1]], str(i[0] + 1)]) for i in temp]
     allPossibleMoves.sort()
-    return allPossibleMovesdef getKnightMoves(pos, chessBoard):
+    return allPossibleMoves
+def getRookMoves(pos, chessBoard):
     column, row = list(pos.strip().lower())
     row = int(row) - 1
-   def getrook
+    column = chess_map_from_alpha_to_index[column]
+    i,j = row, column
+    solutionMoves = []
+
+    # Compute the moves in Rank
+    for j in xrange(8):
+        if j != column:
+            solutionMoves.append((row, j))
+
+    # Compute the moves in File
+    for i in xrange(8):
+        if i != row:
+            solutionMoves.append((i, column))
+
+    solutionMoves = ["".join([chess_map_from_index_to_alpha[i[1]], str(i[0] + 1)]) for i in solutionMoves]
+    solutionMoves.sort()
+    return solutionMoves
+
+def getBishopMoves(pos, chessBoard):
+    
+    row = int(row) - 1
     column = chess_map_from_alpha_to_index[column]
     i,j = row, column
     solutionMoves = []
@@ -116,10 +141,3 @@ def getKnightMoves(pos, chessBoard):
         solutionMoves.append([i - 1, j - 2])
     except:
         pass
-
-    # Filter all negative values
-    temp = [i for i in solutionMoves if i[0] >=0 and i[1] >=0]
-    allPossibleMoves = ["".join([chess_map_from_index_to_alpha[i[1]], str(i[0] + 1)]) for i in temp]
-    allPossibleMoves.sort()
-    return allPossibleMoves
- 
